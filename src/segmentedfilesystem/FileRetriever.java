767301496a6c6e80899090c42a618f664ce468bc
Johannes Martinez <mart4695@morris.umn.edu>
2020-10-29 11:48:35 -0500

    Comment unused import (may be used in the future)

--- src/segmentedfilesystem/FileRetriever.java
+++ src/segmentedfilesystem/FileRetriever.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
+// import java.net.SocketException;
 
 public class FileRetriever {
 
